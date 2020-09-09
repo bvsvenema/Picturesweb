1fee71d4a52d3e7f9912faf88fcb96dd2dd373bc
Benjamin Venema <benjamin.venema001@fclive.nl>
2020-09-09 14:28:23 +0200

    upload test

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <meta charset="utf-8">
+        <meta name="Description" content="this is example of a meta discription. this will often show up in search result">
+        <meta name="Vieuwport" content="widht=device-width, inital-scale=1">
+        <title>yeetus</title>
+    </head>
+    <body>
+
+    
+
+        <?php
+           /* $x = 100;
+
+            function newFunction($x) {
+                $newnr = $x * 0.75;
+                echo " here is 75% of what this :".$newnr;
+            }
+
+            newFunction($x);
+            */
+        ?>
+    </body>
+</html>
